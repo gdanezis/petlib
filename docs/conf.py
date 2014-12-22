af8839cc35c3e8351cc236f4bf8c08b6994be07b
George Danezis <g.danezis@ucl.ac.uk>
2014-12-22 23:33:30 +0000

    Added nice docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -40,14 +40,23 @@ sys.path.insert(0,os.path.abspath(r".."))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.autodoc', 
-    'sphinxcontrib.napoleon',
-]
+try:
+  import sphinxcontrib.napoleon
+  extensions = [
+      'sphinx.ext.autodoc',
+      'sphinx.ext.coverage',
+      'sphinx.ext.mathjax',
+      'sphinx.ext.viewcode',
+      'sphinxcontrib.napoleon',
+  ]
+except:
+  extensions = [
+      'sphinx.ext.autodoc',
+      'sphinx.ext.coverage',
+      'sphinx.ext.mathjax',
+      'sphinx.ext.viewcode',
+  ]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
