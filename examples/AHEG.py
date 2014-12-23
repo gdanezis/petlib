48dc65e5ce594259bb65e099d19d559d277257ff
George Danezis <g.danezis@ucl.ac.uk>
2014-12-23 00:27:11 +0000

    Removed extra params

--- examples/AHEG.py
+++ examples/AHEG.py
@@ -27,13 +27,13 @@ def enc(params, pub, counter):
     b = k * pub + counter * g
     return (a, b)
 
-def add(params, c1, c2):
+def add(c1, c2):
     """Add two encrypted counters"""
     a1, b1 = c1
     a2, b2 = c2
     return (a1 + a2, b1 + b2)
 
-def mul(params, c1, val):
+def mul(c1, val):
     """Multiplies an encrypted counter by a public value"""
     a1, b1 = c1
     return (val*a1, val*b1)
@@ -41,7 +41,7 @@ def mul(params, c1, val):
 def randomize(params, pub, c1):
     """Rerandomize an encrypted counter"""
     zero = enc(params, pub, 0)
-    return add(params, c1, zero)
+    return add(c1, zero)
 
 def make_table(params):
     """Make a decryption table"""
@@ -68,11 +68,11 @@ if __name__ == "__main__":
     assert dec(params, table, priv, one) == 1
 
     # Check addition
-    tmp = add(params, one, one)
+    tmp = add(one, one)
     two = randomize(params, pub, tmp)
     assert dec(params, table, priv, two) == 2
 
     # Check multiplication
-    tmp1 = mul(params, two, 2)
+    tmp1 = mul(two, 2)
     four = randomize(params, pub, tmp1)
     assert dec(params, table, priv, four) == 4
