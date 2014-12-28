859469d349203bd7420c4d4c415bc197d3c371a5
George Danezis <g.danezis@ucl.ac.uk>
2014-12-28 14:25:22 +0000

    Added timing program to identify good curves

---
+++ examples/EcGroup_timing.py
@@ -0,0 +1,34 @@
+from petlib.ec import EcGroup, EcPt
+from petlib.bn import Bn
+import time
+
+if __name__ == "__main__":
+    fails = 0
+    print "List of curves passing the constant-time scalar mult test:"
+    for (nid, name) in sorted(EcGroup.list_curves().items()):
+        G = EcGroup(nid)
+        g = G.generator()
+        order = G.order()
+        h = order.random() * g
+
+        repreats = 100
+        t = []
+        for x in range(0, 400, 100):
+            o = Bn(2) ** x
+            tests = [o.random() for _ in range(repreats)]
+
+            t0 = time.clock()
+            for y in tests:
+                y * h
+            t += [time.clock() - t0]
+            # print x, t[-1] / repreats
+        res = abs(t[0] - t[-1]) < 1.0 / 100
+        if res:
+
+            ps = 1.0 / (t[-1] / repreats)
+            res = ["FAIL", "PASS"][res]
+            print "%3d\t%s\t%2.1f/s\t%s" % (nid, res, ps, name)
+        else:
+            fails += 1
+
+    print "%d fails" % fails
