672cba169e8d458d743decbd2fa5cc25bc71641c
George Danezis <g.danezis@ucl.ac.uk>
2015-01-13 19:28:04 +0000

    Added a mock future to the docs

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['cffi', 'pytest', 'builtins']
+MOCK_MODULES = ['cffi', 'pytest', 'builtins', 'future']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
