3cf75c0962c433c889acf889ae935d225e711a17
George Danezis <g.danezis@ucl.ac.uk>
2015-01-19 00:50:57 +0000

    Get the index back in the toc

--- docs/conf.py
+++ docs/conf.py
@@ -211,13 +211,13 @@ htmlhelp_basename = 'petlibdoc'
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
 #'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
-#'preamble': '',
+'preamble': '\setcounter{tocdepth}{2}',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
