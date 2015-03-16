23f709e483bc7b0dfa15da8207ddc509715ebaa0
George Danezis <g.danezis@ucl.ac.uk>
2015-03-16 13:13:28 +0000

    Make a petlib.run_tests() function that tests an install

--- petlib/__init__.py
+++ petlib/__init__.py
@@ -1,2 +1,21 @@
 # The petlib version
-VERSION = '0.0.25'
+VERSION = '0.0.26'
+
+def run_tests():
+    # These are only needed in case we test
+    import pytest
+    import os.path
+    import glob
+
+    # List all petlib files in the directory
+    petlib_dir = dir = os.path.dirname(os.path.realpath(__file__))
+    pyfiles = glob.glob(os.path.join(petlib_dir, '*.py'))
+    pyfiles = " ".join(pyfiles)
+
+    # Run the test suite
+    print("Directory: %s" % pyfiles)
+    res = pytest.main("-v -x %s" % pyfiles)
+    print("Result: %s" % res)
+
+    # Return exit result
+    return res
